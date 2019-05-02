c61263c7b794b3378502d968c62d7ca70bf43517
FengchenX <fengchenxiaonvsheng@hotmail.com>
2019-05-02 23:29:05 +0800

    add

--- src/service/service.go
+++ src/service/service.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"db/etcd"
-	"db/pg"
 	"github.com/kataras/iris"
 	"router"
 )
@@ -10,9 +8,6 @@ import (
 type Svc struct {
 	iris.Application
 	Router  *router.Router
-	Dynamic *etcd.Client
-	SysDB   *pg.SysDB
-	AuthDB  *pg.AuthDB
 }
 
 func NewSvc() *Svc {
@@ -22,9 +17,8 @@ func NewSvc() *Svc {
 func DefaultSvc() *Svc {
 	svc := NewSvc()
 	svc.Application = *iris.Default()
-	svc.AuthDB = pg.DefaultAuthDB()
-	svc.SysDB = pg.DefaultSysDB()
-	svc.Dynamic = etcd.DefaultCli()
+
+	svc.Use()
 	svc.Router = router.NewRouter(&svc.Application)
 	return svc
 }
