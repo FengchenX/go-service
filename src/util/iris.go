25ec3363dd6e33ad7185d6f19ca795dd496d9520
FengchenX <fengchenxiaonvsheng@hotmail.com>
2019-05-12 12:06:55 +0800

    add

--- src/util/iris.go
+++ src/util/iris.go
@@ -4,15 +4,15 @@ import "github.com/kataras/iris"
 
 func Success(c iris.Context, data interface{}) {
 	c.JSON(iris.Map{
-		"Code": 0,
-		"Msg":  "success",
-		"Data": data,
+		"code": 0,
+		"msg":  "success",
+		"data": data,
 	})
 }
 func Fail(c iris.Context, err error) {
 	c.JSON(iris.Map{
-		"Code": -1,
-		"Msg":  err.Error(),
-		"Data": nil,
+		"code": -1,
+		"msg":  err.Error(),
+		"data": nil,
 	})
 }
