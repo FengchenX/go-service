46f37a16067552ff1d28a24ff89abaca67c88c95
FengchenX <fengchenxiaonvsheng@hotmail.com>
2019-05-12 10:48:06 +0800

    add

--- src/util/pagination.go
+++ src/util/pagination.go
@@ -1,42 +1,50 @@
 package util
 
-//import (
-//	"fmt"
-//	"github.com/gin-gonic/gin"
-//)
-//
-//// page filter
-//// swagger:model
-//type PageFilter struct {
-//	Sort   string `json:"sort"`
-//	Order  string `json:"order"`
-//	Limit  string `json:"limit"`
-//	Offset string `json:"offset"`
-//}
-//
-//func ParsePageFilter(c *gin.Context) (*PageFilter, error) {
-//	var filter PageFilter
-//	e := c.ShouldBindQuery(&filter)
-//	return &filter, e
-//}
-//
-//func PageFilterSql(sql, keyCol string, filter *PageFilter) string {
-//	if len(sql) == 0 || filter == nil {
-//		return sql
-//	}
-//	if len(filter.Sort) > 0 && len(filter.Order) == 0 {
-//		filter.Order = "DESC"
-//	}
-//	if len(filter.Sort) > 0 && filter.Sort != keyCol {
-//		sql = fmt.Sprintf("%s ORDER BY %s %s", sql, filter.Sort, filter.Order)
-//		if len(keyCol) > 0 {
-//			sql = fmt.Sprintf("%s, %s %s", sql, keyCol, filter.Order)
-//		}
-//	} else if len(keyCol) > 0 {
-//		sql = fmt.Sprintf("%s ORDER BY %s %s", sql, keyCol, filter.Order)
-//	}
-//	if len(filter.Offset) > 0 && len(filter.Limit) > 0 {
-//		sql = fmt.Sprintf("%s LIMIT %s OFFSET %s", sql, filter.Limit, filter.Offset)
-//	}
-//	return sql
-//}
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/kataras/iris"
+)
+
+// page filter
+// swagger:model
+type PageFilter struct {
+	Sort   string `json:"sort"`
+	Order  string `json:"order"`
+	Limit  string `json:"limit"`
+	Offset string `json:"offset"`
+}
+
+func ParsePageFilter(c iris.Context) *PageFilter {
+	filter := PageFilter{
+		c.URLParam("sort"),
+		c.URLParam("order"),
+		c.URLParam("limit"),
+		c.URLParam("offset"),
+	}
+	if len(filter.Sort) == 0 && len(filter.Order) == 0 && len(filter.Limit) == 0 && len(filter.Offset) == 0 {
+		return nil
+	}
+	return &filter
+}
+
+func PageFilterSql(sql *gorm.DB, keyCol string, filter *PageFilter) *gorm.DB {
+	if sql ==nil || filter == nil {
+		return sql
+	}
+	if len(filter.Sort) > 0 && len(filter.Order) == 0 {
+		filter.Order = "DESC"
+	}
+	if len(filter.Sort) > 0 && filter.Sort != keyCol {
+		sql = sql.Order(fmt.Sprintf("%s %s", filter.Sort, filter.Order))
+		if len(keyCol) > 0 {
+			sql = sql.Order(keyCol)
+		}
+	} else if len(keyCol) > 0 {
+		sql = sql.Order(fmt.Sprintf("%s %s", keyCol, filter.Order))
+	}
+	if len(filter.Offset) > 0 && len(filter.Limit) > 0 {
+		sql = sql.Limit(filter.Limit).Offset(filter.Offset)
+	}
+	return sql
+}
